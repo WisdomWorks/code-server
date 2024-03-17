b65c03f609b83e21a546b6f99b5384ffcccde5fa
Khiem Ton <th4tkh13m@protonmail.com>
2024-03-17 20:40:44 +0700

    feat: easier Tcp Client

--- client/src/main/java/com/example/code_server/client/ClientApplication.java
+++ client/src/main/java/com/example/code_server/client/ClientApplication.java
@@ -1,6 +1,15 @@
 package com.example.code_server.client;
 
 import com.example.code_server.client.handler.ClientHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.compression.ZlibCodecFactory;
+import io.netty.handler.codec.compression.ZlibWrapper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.netty.bootstrap.Bootstrap;
@@ -10,6 +19,9 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
+import reactor.netty.tcp.TcpClient;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -18,26 +30,54 @@ public class ClientApplication {
 		String host = "127.0.0.1";
 		int port = 8080;
 
-		EventLoopGroup group = new NioEventLoopGroup();
-		try {
-			Bootstrap bootstrap = new Bootstrap();
-			bootstrap.group(group)
-					.channel(NioSocketChannel.class)
-					.option(ChannelOption.TCP_NODELAY, true)
-					.handler(new ChannelInitializer<SocketChannel>() {
-						@Override
-						public void initChannel(SocketChannel ch) throws Exception {
-							ch.pipeline().addLast(new ClientHandler());
-						}
-					});
+		Connection connection =
+				TcpClient.create()
+						.host(host)
+						.port(port)
+						.connectNow();
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode packet = JsonNodeFactory.instance.objectNode();
 
-			// Connect to the server
-			bootstrap.connect(host, port).sync().channel().closeFuture().sync();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			group.shutdownGracefully();
-		}
+		packet.put("name", "submission-request");
+		packet.put("submission-id", "1");
+		packet.put("problem-id", "1");
+		packet.put("language", "java");
+		packet.put("source", "source");
+		packet.put("judge-id", "1");
+		packet.put("priority", 0);
+
+		// Convert JSON object to string
+		String packetString = mapper.writeValueAsString(packet);
+		ByteBuf buffer = Unpooled.buffer();
+		buffer.writeBytes(packetString.getBytes());
+		connection
+				.outbound()
+				.send(Mono.just(buffer))
+				.then()
+				.subscribe();
+
+		connection.inbound()
+				.receive()
+				.asString()
+				.take(1) // Take only one response
+				.doOnTerminate(connection::dispose) // Dispose the connection after receiving the response
+				.subscribe(response -> {
+					try {
+						System.out.println("Received data from server: " + response);
+						// Parse the JSON response
+						JsonNode jsonResponse = mapper.readTree(response);
+
+						// Process the JSON response as needed
+						// ...
+
+					} catch (JsonProcessingException e) {
+						// Handle JSON parsing exception
+						e.printStackTrace();
+					}
+				});
+
+		connection.onDispose()
+				.block();
+
 	}
 }
-
